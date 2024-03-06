be51ff0d435eb056021b0874774d4e75a0e81b08
unknown <assisholandal@gmail.com>
2024-03-05 22:47:37 -0300

    Implementado a configuração de receber notificação utilizando o serviço da google e onesignal

--- App.tsx
+++ App.tsx
@@ -2,12 +2,17 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { OneSignal } from 'react-native-onesignal';
+
 import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize("f1571180-5a0c-48ad-95d3-788a32be45cb");
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
